e0ac5fee2b0805c80f459ddaede0ac7e7ac6e402
lucascohen <luqas.c@gmail.com>
2022-03-28 21:38:33 +0300

    Simplified code

--- app/dots.py
+++ app/dots.py
@@ -5,7 +5,6 @@ from typing import Optional
 import pygame
 
 from app.entities.line import Line
-from app.utils.utils import print_score_and_turns
 from entities.colors import BaseColorTheme
 from entities.grid import Grid
 # Create logger and get constants
@@ -25,7 +24,7 @@ grid = Grid(size=6)
 
 # Game loop
 screen.fill(BaseColorTheme['BG'])
-grid.render(window=screen)
+# grid.render(window=screen)
 running: int = True
 score: int = 0
 turns: int = 30
@@ -34,42 +33,44 @@ while running:
     # 1 Process input/events
     clock.tick(FPS)  # will make the loop run at the same speed all the time
     mouse_pos, running, line = check_events(line)
-    if line is None:
-        logger.debug("Generating New Line")
-        line = Line()
 
-    # # 2 Update
-    # # Select dot that is in mouse position
-    # TODO: Check for efficiency
-    for dot in grid.dots.reshape(-1):  # We flatten the 2d array to iterate over it in a simpler manner
-        if dot.rect.collidepoint(mouse_pos):
-            logger.debug(f"Mouse in Dot {dot.x_ind}, {dot.y_ind}")
-            # Select dot and append to line
-            if line.append(dot.select()):
-                logger.info(f"Line is {len(line.nodes)} dots long.")
-
-    # 3 Draw/render
-    screen.fill(BaseColorTheme['BG'])
-    grid.render(window=screen)
-
-    # Draw Line from last selected dot to current mouse position of there is a line
-    if len(line) > 0:
-        line.render(screen)
-
-    # If line end then remove dots, and update with new dots
-    if line.end:
-        grid.remove(line.nodes)
-        grid.update()
-        if line.valid:
-            score += len(line)
-            turns -= 1
-        logger.info(f"\tScore: {score} points! \n\t Turns Left {turns}")
-        line = None
-
-    # Print score
-    print_score_and_turns(screen, score, turns)
-
-    # Done after drawing everything to the screen
-    pygame.display.flip()
+    grid.render(screen)
+    # if line is None:
+    #     logger.debug("Generating New Line")
+    #     line = Line()
+    #
+    # # # 2 Update
+    # # # Select dot that is in mouse position
+    # # TODO: Check for efficiency
+    # for dot in grid.dots.reshape(-1):  # We flatten the 2d array to iterate over it in a simpler manner
+    #     if dot.rect.collidepoint(mouse_pos):
+    #         logger.debug(f"Mouse in Dot {dot.x_ind}, {dot.y_ind}")
+    #         # Select dot and append to line
+    #         if line.append(dot.select()):
+    #             logger.info(f"Line is {len(line.nodes)} dots long.")
+    #
+    # # 3 Draw/render
+    # screen.fill(BaseColorTheme['BG'])
+    # grid.render(window=screen)
+    #
+    # # Draw Line from last selected dot to current mouse position of there is a line
+    # if len(line) > 0:
+    #     line.render(screen)
+    #
+    # # If line end then remove dots, and update with new dots
+    # if line.end:
+    #     grid.remove(line.nodes)
+    #     grid.update()
+    #     if line.valid:
+    #         score += len(line)
+    #         turns -= 1
+    #     logger.info(f"\tScore: {score} points! \n\t Turns Left {turns}")
+    #     line = None
+    #
+    # # Print score
+    # print_score_and_turns(screen, score, turns)
+    #
+    # # Done after drawing everything to the screen
+    # pygame.display.flip()
 
 pygame.quit()
--- app/entities/grid.py
+++ app/entities/grid.py
@@ -1,24 +1,77 @@
-from typing import Optional
+from typing import Optional, Dict
 
 import numpy as np
 import pygame
 
+from app.entities.colors import colorType, BaseColorTheme
 from app.entities.line import Line
 
 
 class Grid:
-    def __init__(self, size: int = 6, screen: Optional[pygame.Surface] = None,
-                 screen_dim: int = 500, fps: int = 30):
+    def __init__(self, size: int = 6,
+                 screen: Optional[pygame.Surface] = None,
+                 screen_dim: int = 500,
+                 color_theme: Dict[int, colorType] = BaseColorTheme
+                 ):
         self.grid_size: int = size
-        self.dots: np.ndarray = np.random.randint(1, 6, (6, 6))
         self.screen: Optional[pygame.Surface] = screen
         self.screen_dim: int = screen_dim
-        self.fps: int = fps
+        self.color_theme: Dict[int | str, colorType] = color_theme
+        self.dots: np.ndarray = np.random.randint(1, 6, (size, size))
         self.score: int = 0
         self.turns_left: int = 30
 
     def render(self, line: Optional[Line] = None):
-        pass
+        if self.screen is None:
+            self._init_screen()
+        self._render_score_and_turns()
+        self._render_dots()
+        self._render_line(line)
+
+        pygame.display.flip()
 
     def update(self):
+        pass
+
+    def _init_screen(self):
+        pygame.init()
+        pygame.display.init()
+        pygame.display.set_caption("Dots")
+        self.screen = pygame.display.set_mode((self.screen_dim, self.screen_dim * 1.2))
+        self.screen.fill(BaseColorTheme['BG'])
+
+    def _render_score_and_turns(self):
+        width, starting_height = self.screen_dim, self.screen_dim
+        height = self.screen_dim * 0.2
+
+        font_small: pygame.font.Font = pygame.font.SysFont("arial", int(width / 20))
+        font_big: pygame.font.Font = pygame.font.SysFont("arial_bold", int(width / 10))
+
+        score_title = font_small.render("SCORE", True, self.color_theme['text'])
+        score_title_rect = score_title.get_rect()
+        score_title_rect.center = (width // 4, starting_height)
+        self.screen.blit(score_title, score_title_rect)
+
+        score_number = font_big.render(f"{self.score}", True, self.color_theme['text'])
+        score_number_rect = score_number.get_rect()
+        score_number_rect.center = (width // 4, starting_height + score_title_rect.height * 1.2)
+        self.screen.blit(score_number, score_number_rect)
+
+        turns_title = font_small.render("MOVES", True, self.color_theme['text'])
+        turns_title_rect = turns_title.get_rect()
+        turns_title_rect.center = (3 * (width // 4), starting_height)
+        self.screen.blit(turns_title, turns_title_rect)
+
+        turns_number = font_big.render(f"{self.turns_left}", True, self.color_theme['text'])
+        turns_number_rect = turns_number.get_rect()
+        turns_number_rect.center = (3 * (width // 4), starting_height + turns_title_rect.height * 1.2)
+        self.screen.blit(turns_number, turns_number_rect)
+
+        pygame.draw.line(self.screen, self.color_theme['text'], (width // 2, starting_height - score_title_rect.height),
+                         (width // 2, starting_height + 0.8 * height), width=2)
+
+    def _render_line(self, line: Line):
+        pass
+
+    def _render_dots(self):
         pass
